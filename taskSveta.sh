bbcc55da46e087d46b09d472806e84063cbe4b26
SvetlanaFranchuk <139387166+SvetlanaFranchuk@users.noreply.github.com>
2023-09-24 17:31:55 +0200

    Add files via upload

--- taskSveta.sh
+++ taskSveta.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#прошу смотреть выполнение задания и папку архивации по пути, указанному ниже
 todayDate=date +%d.%m.%Y
 
 	for i in{1..10}
 	do
-	touch $i$todayDate
+	touch /opt/110723_morning/Franchuk/$i$todayDate
 	done
